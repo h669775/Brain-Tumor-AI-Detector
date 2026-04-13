6aff2201bce91961c0c7b02b05afc0fed8bb0c6c
h669775 <111428344+h669775@users.noreply.github.com>
2026-04-13 16:13:08 +0200

    Updated mode_loader
    Error exception

--- backend/services/model_loader.py
+++ backend/services/model_loader.py
@@ -2,27 +2,30 @@ import os
 import json
 import tensorflow as tf
 
-# Get backend root folder
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-# Paths to your files
 MODEL_PATH = os.path.join(BASE_DIR, "models", "resnet50_best.keras")
 CLASS_NAMES_PATH = os.path.join(BASE_DIR, "models", "class_names.json")
 
-# Cache variables (so we don't reload every time)
 _model = None
 _class_names = None
-
 
 def load_model_and_labels():
     global _model, _class_names
 
-    # Load model only once
+    if not os.path.exists(MODEL_PATH):
+        raise FileNotFoundError(
+            "Model file not found. Download 'resnet50_best.keras' from GitHub Releases and place it in backend/models/."
+        )
+
+    if not os.path.exists(CLASS_NAMES_PATH):
+        raise FileNotFoundError(
+            "class_names.json not found in backend/models/."
+        )
+
     if _model is None:
         print("Loading model...")
         _model = tf.keras.models.load_model(MODEL_PATH)
 
-    # Load class names only once
     if _class_names is None:
         print("Loading class names...")
         with open(CLASS_NAMES_PATH, "r") as f:
