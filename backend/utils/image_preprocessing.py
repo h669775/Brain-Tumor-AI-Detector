9e9d8b99cb7e7b13cf3019e2373052a665fd0f53
h669775 <111428344+h669775@users.noreply.github.com>
2026-04-29 23:47:48 +0200

    Fixed double image preprocessing

--- backend/utils/image_preprocessing.py
+++ backend/utils/image_preprocessing.py
@@ -1,14 +1,11 @@
 from PIL import Image
 import numpy as np
-import tensorflow as tf
 
 def preprocess_uploaded_file(file_storage):
     image = Image.open(file_storage.stream).convert("RGB")
     image = image.resize((256, 256))
 
     image_array = np.array(image, dtype=np.float32)
-
-    image_array = tf.keras.applications.resnet.preprocess_input(image_array)
     image_array = np.expand_dims(image_array, axis=0)
 
     return image_array
